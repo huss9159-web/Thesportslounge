fad050331b0307bc1163b4c6b65e2aa1e834e3d7
Hussain Shabbir <hussain@abweb.com.au>
2025-11-10 10:26:24 +0500

    added

--- api/bookings/[id].js
+++ api/bookings/[id].js
@@ -1,5 +1,5 @@
 // api/bookings/[id].js
-import { connectDB } from "../../../db.js";
+import { connectDB } from "../../db.js";
 import Booking from "../../../models/Booking.js";
 
 export default async function handler(req, res) {
